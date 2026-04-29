f79b39cd11173694d31052f04c2962fd83d525f1
Aron Lukacs <aron.lukacs@gmail.com>
2026-04-29 10:44:13 +0200

    feat(styles): add benefit-demos stylesheet for interactive USP cards

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/main.css'
 import './styles/animations.css'
+import './styles/benefit-demos.css'
 import LenisProvider from './providers/LenisProvider'
 import App from './App.jsx'
 
